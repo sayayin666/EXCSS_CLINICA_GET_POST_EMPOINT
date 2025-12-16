c288aa5b8c8f09805b011a42cc079f45bdf70af9
sayayin666 <sayayinjohan4@gmail.com>
2025-12-15 20:57:43 -0500

    cambio correcto

--- clinica_get_post/src/Clinica.jsx
+++ clinica_get_post/src/Clinica.jsx
@@ -14,7 +14,7 @@ function Clinica() {
                 apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrb2pyeWF4YnF1cXR3dnV5aGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MTQ0MTUsImV4cCI6MjA3MzA5MDQxNX0.nZMSWKNIve_UmSe1KEehy9ocL2FIR25QflnccDRQ998",
                 Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrb2pyeWF4YnF1cXR3dnV5aGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc1MTQ0MTUsImV4cCI6MjA3MzA5MDQxNX0.nZMSWKNIve_UmSe1KEehy9ocL2FIR25QflnccDRQ998"
             },
-            body: JSON.stringify({el_nombre: nombre, la_ciudad: ciudad})
+            body: JSON.stringify({nombre: nombre, ciudad: ciudad})
         })
     }
     useEffect(() => {
